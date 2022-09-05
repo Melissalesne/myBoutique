cea0e8f367aeca31d28a6fe1520dbdb7bed86c11
Melissa Lesne <lesnemelissa2@gmail.com>
2022-09-05 10:21:30 +0200

    rectification E4

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -3,6 +3,9 @@ import CategoryCards from "../Components/CategoryCards";
 import "./homeScreen.css";
 
 export default function HomeScreen() {
+  function handleSubmit(evt) {
+    evt.preventDefault();
+  }
   return (
     <>
       <h1>La boutique de John Doe</h1>
@@ -27,7 +30,45 @@ export default function HomeScreen() {
           description={"La mode pour eux aussi"}
           image={"./assets/images/baby.jpg"}
         />
+        <br />
       </div>
+      <form onSubmit={(evt) => handleSubmit(evt)}>
+        <div className="container mt-5 bg-dark rounded-3">
+          <h3 className="text-center mt-5 text-white">Contactez-Nous...</h3>
+          <div className="mb-3 w-50 m-auto">
+            <label htmlFor="name">Nom: </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Nom, Prénom"
+            />
+          </div>
+          <div className="mb-3 w-50 m-auto">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="adress@gmail.com"
+            />
+          </div>
+          <div className="mb-3 w-50 m-auto">
+            <label htmlFor="Textarea">Textarea:</label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              placeholder="Sujet"
+            ></textarea>
+            <input
+              type="submit"
+              value="envoyer"
+              className="btn btn-primary mt-3 d-flex m-auto"
+            />
+          </div>
+        </div>
+      </form>
     </>
   );
 }
